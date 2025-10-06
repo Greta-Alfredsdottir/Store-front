87d7a6d69069f6f88b1d26554f15a08f3cdf7dba
Greta <asth0010@elev.techcollege.dk>
2025-10-06 09:23:19 +0200

    new

---
+++ Assets/JS/module/module.js
@@ -0,0 +1,15 @@
+// Her har vi vores modules
+
+
+// #region controller Code,
+
+function getdata() {
+    
+};
+// #endregion 
+
+//#region Modul code
+//#endregion
+
+//#region View code
+//#endregion
